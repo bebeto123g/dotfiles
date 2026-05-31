babe18cb0d1f33e3e4af00ebd34766130e58f5b0
bebeto@arch-honor-14 <bebeto123g@gmail.com>
2026-05-31 12:09:03 +0300

    rebase

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source ~/.dotfiles/.config/fish/conf.d/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+#
+export EDITOR=vim
